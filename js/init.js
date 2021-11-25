60f60227b98e7d1619a4d860bbb5981d74cd2082
pwodj801 <95043978+pwodj801@users.noreply.github.com>
2021-11-25 13:50:14 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDSIMfzrsQZUuqGNdqvvrJADd9UPcv7BZI",
+  authDomain: "pwodj801-5f675.firebaseapp.com",
+  projectId: "pwodj801-5f675",
+  storageBucket: "pwodj801-5f675.appspot.com",
+  messagingSenderId: "32728671521",
+  appId: "1:32728671521:web:06a7e7c43252425d8d0a60",
+  measurementId: "G-K2FXSQRX7S"
+});
